f0bd2cd9e6bb9ebdd99ae992f7be0ba5254b92b0
Анастасия Скульская <askulskaya@atsaero.ru>
2025-09-03 19:14:40 +0300

    Инкремент 8

---
+++ internal/middleware/request_compressor.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"github.com/acya-skulskaya/shortener/internal/helpers"
+	"net/http"
+	"strings"
+)
+
+/*
+Добавьте поддержку gzip в ваш сервис. Научите его:
+
+    Принимать запросы в сжатом формате (с HTTP-заголовком Content-Encoding).
+    Отдавать сжатый ответ клиенту, который поддерживает обработку сжатых ответов (с HTTP-заголовком Accept-Encoding).
+
+Функция сжатия должна работать для контента с типами application/json и text/html.
+Вспомните middleware из урока про HTTP-сервер, это может вам помочь.
+*/
+
+func RequestCompressor(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
+		// который будем передавать следующей функции
+		ow := w
+
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
+			cw := helpers.NewCompressWriter(w)
+			// меняем оригинальный http.ResponseWriter на новый
+			ow = cw
+			// не забываем отправить клиенту все сжатые данные после завершения middleware
+			defer cw.Close()
+		}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := contentEncoding == "application/json" || contentEncoding == "text/html"
+		if sendsGzip {
+			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
+			cr, err := helpers.NewCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// меняем тело запроса на новое
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		// передаём управление хендлеру
+		next.ServeHTTP(ow, r)
+	})
+}
