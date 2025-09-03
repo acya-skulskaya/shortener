1fbf47188dd13e9a09f94c111e302543c403d427
Анастасия Скульская <askulskaya@atsaero.ru>
2025-09-03 21:14:03 +0300

    Инкремент 8

--- internal/middleware/request_compressor.go
+++ internal/middleware/request_compressor.go
@@ -6,16 +6,6 @@ import (
 	"strings"
 )
 
-/*
-Добавьте поддержку gzip в ваш сервис. Научите его:
-
-    Принимать запросы в сжатом формате (с HTTP-заголовком Content-Encoding).
-    Отдавать сжатый ответ клиенту, который поддерживает обработку сжатых ответов (с HTTP-заголовком Accept-Encoding).
-
-Функция сжатия должна работать для контента с типами application/json и text/html.
-Вспомните middleware из урока про HTTP-сервер, это может вам помочь.
-*/
-
 func RequestCompressor(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
