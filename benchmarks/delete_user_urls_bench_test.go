5ab984a0acce725e2868513eae745628a0aaaffe
Анастасия Скульская <askulskaya@atsaero.ru>
2026-01-19 19:31:33 +0300

    Инкремент 18

--- benchmarks/delete_user_urls_bench_test.go
+++ benchmarks/delete_user_urls_bench_test.go
@@ -1,28 +1,19 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"testing"
-
-	shorturlinmemory "github.com/acya-skulskaya/shortener/internal/repository/short_url_in_memory"
-)
-
-func BenchmarkDeleteUserURLs(b *testing.B) {
-	repo := shorturlinmemory.InMemoryShortURLRepository{}
-	_, users, usersToURLs := seedShortUrls(&repo, 10000)
-
-	b.ResetTimer()
-	j := 0
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		if j >= len(users) {
-			fmt.Println(i)
-			j = 0
-			_, users, usersToURLs = seedShortUrls(&repo, 10000)
-		}
-		b.StartTimer()
-		repo.DeleteUserUrls(context.Background(), usersToURLs[users[j]], users[j])
-		j++
-	}
-}
+//func BenchmarkDeleteUserURLs(b *testing.B) {
+//	repo := shorturlinmemory.InMemoryShortURLRepository{}
+//	_, users, usersToURLs := seedShortUrls(&repo, 10000)
+//
+//	b.ResetTimer()
+//	j := 0
+//	for i := 0; i < b.N; i++ {
+//		b.StopTimer()
+//		if j >= len(users) {
+//			j = 0
+//			_, users, usersToURLs = seedShortUrls(&repo, 10000)
+//		}
+//		b.StartTimer()
+//		repo.DeleteUserUrls(context.Background(), usersToURLs[users[j]], users[j])
+//		j++
+//	}
+//}
